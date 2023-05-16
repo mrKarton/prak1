be8330a25217aa42047da97afdee7b3cc9d2ea69
mrKarton <gornostaev.dmitry04@gmail.com>
2023-05-17 00:37:49 +0400

    Initial

---
+++ Practice 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Practice_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 100; i++)
+            {
+                if (i % 3 == 0)
+                    Console.Write("Fizz");
+                if (i % 5 == 0)
+                    Console.Write("Buzz");
+                Console.WriteLine("");
+            }
+        }
+    }
+}
